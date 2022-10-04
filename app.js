2bf9dda073d03c3bd1517824b08d09954a0e7e10
nmfkimball <114933412+nmfkimball@users.noreply.github.com>
2022-10-04 15:21:46 -0500

    added router

--- app.js
+++ app.js
@@ -1,46 +1,17 @@
-const express = require("express")
-const app = express()
-const queries = require("./queries")
+const express = require('express');
+const app = express();
+const queries = require('./queries');
+const gamesRoutes = require('./routes/games');
 
-app.use(express.json())
+app.use(express.json());
 
-app.get("/games", (request, response) => {
-    queries.list().then(games => {
-        response.json({games});
-    }).catch(console.error);
-});
-
-app.get("/games/:id", (request, response) => {
-    queries.read(request.params.id).then(game => {
-        game
-            ? response.json({game})
-            : response.sendStatus(404)
-    }).catch(console.error);
-});
-
-app.post("/games", (request, response) => {
-    queries.create(request.body).then(game => {
-        response.status(201).json({game});
-    }).catch(console.error);
-});
-
-app.delete("/games/:id", (request, response) => {
-    queries.delete(request.params.id).then(() => {
-        response.sendStatus(204);
-    }).catch(console.error);
-});
-
-app.put("/games/:id", (request, response) => {
-    queries.update(request.params.id, request.body).then(game => {
-        response.json({game});
-    }).catch(console.error);
-});
+app.use('/games', gamesRoutes);
 
 app.use((error, request, response, next) => {
-    response.status(500)
-    response.json({
-      error: error.message,
-    })
+  response.status(500);
+  response.json({
+    error: error.message,
+  });
 });
 
-module.exports = app
+module.exports = app;
